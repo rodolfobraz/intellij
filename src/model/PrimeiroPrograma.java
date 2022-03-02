be73a2d5cc710fa6c795f292895f924edfb3da22
Rodolfo Braz <rodolfdobraz@icloud.com>
2022-03-02 09:13:14 -0300

    First commint

---
+++ src/model/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package model;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+/*        int a = 5;
+        int b = 3;
+        System.out.println("Hello World!" + (a+b));*/
+    }
+}
